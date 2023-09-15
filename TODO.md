42933eff283c1846d80ef07ed61b4a7b7f006704
cameronmankin <81383976+cameronmankin@users.noreply.github.com>
2023-09-14 23:20:32 -0500

    Temp type icons, added game-icons to Gemfile

--- TODO.md
+++ TODO.md
@@ -1,7 +1,6 @@
 # TODO
-- Figure out margins
-- Test importing to tabletop sim
-- Figure out how to define our own layout
 - Add all our card info to the layout
 - Add icons and color to cards
 - Pick out icons on Game-icons.net
+- Figure out game-icons implementation that doesn't require hard-coding
+- Make card back png for TTS
--- deck.rb
+++ deck.rb
@@ -1,5 +1,6 @@
 require 'squib'
 require 'open-uri'
+require 'game_icons'
 
 # Open the google sheet
 google_sheet_id = "1bTajO_mbHerEMDwGkJEBR-lDXm1hIUlSEe9IwY4hSDo"
@@ -7,13 +8,18 @@ sheet_gid = "0"
 buffer = URI.open("https://docs.google.com/spreadsheets/d/#{google_sheet_id}/export?format=csv&gid=#{sheet_gid}").read
 data = Squib.csv data: buffer
 
+# Squib merges an array of layouts in sequential order.
+# For now we're grafting onto economy, but eventually we should make our own.
+layouts = ['economy.yml', 'layout.yml']
+
 # Lay out the cards themselves
-Squib::Deck.new cards: data['Name'].size, layout: 'economy.yml' do
+Squib::Deck.new cards: data['Name'].size, layout: layouts do
   background color: 'white'
   rect layout: 'cut' # cut line as defined by TheGameCrafter
   rect layout: 'safe' # safe zone as defined by TheGameCrafter
   text str: data['Name'], layout: 'title'
   text str: data['Text'], layout: 'description'
   text str: Time.now, layout: 'credits'
+  svg layout: data['Type'] # from layout.yml
   save_sheet columns: 4, rows: 4, margin: 0, gap: 0
 end
